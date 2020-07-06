b1db8ebbd3a8c7d1d5c314803f61ef0593cf7b2b
Mahmoud Swehli <mahmoud@moodio.co.uk>
2020-07-06 10:43:23 +0100

    Initial commit

---
+++ Moodio.ProfileUrlsSample/Controllers/ProfilesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moodio.ProfileUrlsSample.Controllers
+{
+    [ApiController]
+    [Route("{profile:regex(^[[@]]{{1}}[[0-9a-zA-Z]]+$)}")]
+    public class ProfilesController : ControllerBase
+    {
+
+        [HttpGet()]
+        public IActionResult GetProfile([FromRoute]string profile)
+        {
+            return new JsonResult(new { ProfileName = profile.Substring(1) });
+        }
+    }
+}
